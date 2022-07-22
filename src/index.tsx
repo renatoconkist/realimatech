f2d163c995142bafd2fc824c7e88de08bd8120f8
Renato Araujo Lima <mail@realima.tech>
2022-07-22 00:11:20 -0300

    firebase cli deploy and github action automation

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// // Import the functions you need from the SDKs you need
+// import firebase from 'firebase/app';
+// import firebaseAnalytics from 'firebase/analytics';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   // databaseURL: process.env.REACT_APP_DB,
+//   projectId: process.env.REACT_APP_PID,
+//   storageBucket: process.env.REACT_APP_SB,
+//   messagingSenderId: process.env.REACT_APP_SID,
+//   appId: process.env.REACT_APP_APPID,
+//   measurementId: process.env.REACT_APP_MID
+// };
+
+// // Initialize Firebase
+// const app = firebase.initializeApp(firebaseConfig);
+// const analytics = firebaseAnalytics.getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
   <React.StrictMode>
     <App />
